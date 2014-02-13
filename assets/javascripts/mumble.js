f94ce785c89ec5a119a1e2208e12adac30fae7c0
Onaldanm <darkkarouko@gmail.com>
2014-02-13 10:28:26 +0100

    Mise en Commentaire

--- assets/javascripts/mumble.js
+++ assets/javascripts/mumble.js
@@ -1,20 +1,20 @@
 (function() {
-	Discourse.HeaderController = Discourse.Controller.extend(Discourse.HeaderController, {
-	  actions: {
-		showMumble: function(headerView) {
-		  var self = this;
+	// Discourse.HeaderController = Discourse.Controller.extend({
+	  // actions: {
+		// showMumble: function(headerView) {
+		  // var self = this;
 
-		  Discourse.ajax("/notifications").then(function(result) {
-			self.set("mumble", result);
-			headerView.showDropdownBySelector("#user-mumble");
-		  });
-		}
-	  }
-  });
+		  // Discourse.ajax("/notifications").then(function(result) {
+			// self.set("mumble", result);
+			// headerView.showDropdownBySelector("#user-mumble");
+		  // });
+		// }
+	  // }
+  // });
   
-  Discourse.HeaderView = Discourse.View.extend(Discourse.HeaderView, {
-	  showMumble: function() {
-		this.get("controller").send("showMumble", this);
-	  }
-  });
+  // Discourse.HeaderView = Discourse.View.extend({
+	  // showMumble: function() {
+		// this.get("controller").send("showMumble", this);
+	  // }
+  // });
 })();
